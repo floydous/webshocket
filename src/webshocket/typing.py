c011a61da19e0eb92d03bb583cebdeeca23dbecf
Floydous <218109625+floydous@users.noreply.github.com>
2025-08-18 17:06:39 +0800

    `RPC_Function` Protocol

--- src/webshocket/typing.py
+++ src/webshocket/typing.py
@@ -1,4 +1,13 @@
-from typing import TypedDict
+from typing import TypedDict, Any, Awaitable, Protocol, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .connection import ClientConnection
+
+DEFAULT_WEBSHOCKET_SUBPROTOCOL = "webshocket.v1"
+
+
+class RPC_Function(Protocol):
+    def __call__(self, connection: "ClientConnection", /, *args: Any, **kwargs: Any) -> Awaitable[Any]: ...
 
 
 class CertificatePaths(TypedDict):
