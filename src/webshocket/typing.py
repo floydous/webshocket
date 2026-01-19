180a4ad682419f706feb468fa145dbe14add8a3c
Floydous <218109625+floydous@users.noreply.github.com>
2026-01-20 07:44:54 +0800

    New: `RateLimitConfig` and `RPC_Method`

--- src/webshocket/typing.py
+++ src/webshocket/typing.py
@@ -1,4 +1,5 @@
-from typing import TypedDict, Any, Awaitable, Protocol, TYPE_CHECKING
+from typing import Callable, Optional, TypedDict, Any, Awaitable, Protocol, TYPE_CHECKING
+from dataclasses import dataclass
 
 if TYPE_CHECKING:
     from .connection import ClientConnection
@@ -8,6 +9,37 @@ DEFAULT_WEBSHOCKET_SUBPROTOCOL = "webshocket.v1"
 
 class RPC_Function(Protocol):
     def __call__(self, connection: "ClientConnection", /, *args: Any, **kwargs: Any) -> Awaitable[Any]: ...
+    @property
+    def __name__(self) -> str: ...
+
+
+class RPC_Predicate(Protocol):
+    def __call__(self, connection: "ClientConnection") -> bool: ...
+
+
+class SessionState(Protocol):
+    """
+    Defines the interface for session state.
+    """
+
+    pass
+
+
+@dataclass(frozen=True)
+class RateLimitConfig:
+    limit: int
+    period: float
+    disconnect_on_limit_exceeded: bool
+
+
+@dataclass(slots=True, frozen=True)
+class RPCMethod:
+    func: Callable
+    rate_limit: Optional[RateLimitConfig] = None
+    restricted: Optional[RPC_Predicate] = None
+
+    def __repr__(self) -> str:
+        return f"RPCMethod(func={self.func.__name__}, rate_limit={self.rate_limit}, restricted={self.restricted})"
 
 
 class CertificatePaths(TypedDict):
